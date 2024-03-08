b54e8d71463553493467f75972a735fc683f1458
NightTimeSky04 <ntsbryant@gmail.com>
2024-03-08 12:59:14 +0000

    Improved comments, prototype collection display (not updating)

--- main.py
+++ main.py
@@ -24,64 +24,131 @@ if os.path.isfile(json_file_name):
 
 else:
 
+    # Read from input file
     with open(txt_file_name) as collection_input:
         collection_list = collection_input.readlines()
 
+    # Dict to contain all collection information
     collection = {}
 
+    # Parse input into collection
     for item in collection_list:
+        # All collections begin empty
         collection[item.strip()] = False
 
+    # Save as new JSON file
     with open(json_file_name, "x") as output_filepath:
         js.dump(collection, output_filepath)
 
 
-# UI must take a user input as items are found, and update the display of which collectables have been found
+# UI takes a user input as items are found, and updates the display of which collectables have been found
 
 # Handle item submission via the GUI
 def submit_item():
     """
-    Check whether a specified item appears in the collection and if it has been collected, then update the collection accordingly and display a descriptive message to the user.
+    Check whether a user specified item appears in the collection and if it has previously been collected, then update the collection accordingly and display a descriptive message to the user.
     """
-    item = ent_user_input.get().title()
+    # Get user input, discarding capitalisation
+    item = ent_user_input.get()
 
-    if item in collection:
+    # Track whether item appears in the collection
+    is_not_in_collection = True
 
-        if not collection.get(item):
-            lbl_output["text"] = f"Found {item}!\n"
-            collection[item] = True
-            with open(json_file_name, "w") as output_filepath:
-                js.dump(collection, output_filepath)
+    for collectable in collection:
 
-        else:
-            lbl_output["text"] = f"{item} already found.\n"
+        if item.lower() == collectable.lower():
+            is_not_in_collection = False  # Item found in the collection
 
-    else:
-        lbl_output["text"] = f"{item} is not in the collection.\n"
+            # Update collection and text output if a new collectable has been found
+            if not collection.get(collectable):
+                lbl_output["text"] = f"Found {collectable}!\n"
 
+                collection[collectable] = True
+
+                # Save changes to collection
+                with open(json_file_name, "w") as output_filepath:
+                    js.dump(collection, output_filepath)
+
+            # Prompt user if the collectable had been found previously
+            else:
+                lbl_output["text"] = f"{collectable} already found.\n"
+
+    # Prompt user if the item is not in the collection
+    if is_not_in_collection:
+        lbl_output["text"] = f"{item.title()} is not in the collection.\n"
+
+    # Remove user input, making space for a new submission
     ent_user_input.delete(0, tk.END)
 
 
+# GUI structure and contents
+
+# Launch an instance of Tk
 window = tk.Tk()
-window.title("Collections")
+window.title("Collections for {game}")
 
 
+# Frame containing widgets for item submission
 frm_submission = tk.Frame(master=window)
-frm_submission.grid(row=0, column=0, padx=20)
+frm_submission.grid(row=0, column=0)
 
+# Label prompt to submit items
 lbl_item_submission = tk.Label(
     master=frm_submission, text="Enter collected item here:")
 lbl_item_submission.grid(row=0, column=0, sticky="e")
 
+# Entry which takes item input
 ent_user_input = tk.Entry(master=frm_submission, width=50)
 ent_user_input.grid(row=0, column=1, sticky="w")
 
+# Submission button which calls submit_item
 btn_submit = tk.Button(master=frm_submission,
                        text="submit", command=submit_item)
-btn_submit.grid(row=0, column=3, padx=5)
+btn_submit.grid(row=0, column=3)
 
-lbl_output = tk.Label(master=window)
-lbl_output.grid(row=1, column=0, sticky="n")
+# Output text to user describing the outcome of their submission
+lbl_output = tk.Label(master=frm_submission)
+lbl_output.grid(row=1, column=1, sticky="n")
+
+
+# Frame containing widgets which display the collection
+frm_collection = tk.Frame(master=window)
+frm_collection.grid(row=1, column=0, padx=10, pady=10)
+
+
+# Number of columns in collection display
+column_count = 4
+# Track position of collectables in list in order to determine row position in diplay
+entry_index = 0
+
+
+# Display grid of collectables, concealing information until they have been found
+for collectable in collection:
+
+    display_text = ""  # Default to no text
+    if collection[collectable]:
+        display_text = collectable
+
+    # Frame for each collectable, contains widgets which display information
+    frm_collectable = tk.Frame(
+        master=frm_collection, borderwidth=4, relief=tk.RAISED)
+
+    # Position frame in grid according to number of columns and entry index
+    row_index = entry_index // column_count
+    column_index = entry_index % column_count
+    frm_collectable.grid(row=row_index, column=column_index)
+
+    # Label to display collectable name
+    lbl_collectable = tk.Label(
+        master=frm_collectable, text=display_text, width=25, relief=tk.GROOVE)
+    lbl_collectable.grid(row=0, column=0)
+
+    # Label to display item description or hint
+    lbl_description = tk.Label(master=frm_collectable,
+                               text="Hints/description here")
+    lbl_description.grid(row=1, column=0)
+
+    entry_index += 1  # Update index tracker
 
 
 window.mainloop()
